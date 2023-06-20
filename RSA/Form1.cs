9a7b3d9df6c6d6ca4d3c1d7e211d7b70c10fbe6a
Süleyman Begic <begic@software.tirol>
2023-06-20 14:54:21 +0200

    first try

--- RSA/Form1.cs
+++ RSA/Form1.cs
@@ -1,23 +1,40 @@
-namespace RSA
+using System.Numerics;
+
+namespace RSA;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Form1()
     {
-        public Form1()
+        InitializeComponent();
+
+        //Zum Entschlüsseln: 102 341 418;
+        //Privatekey(19, 481);
+    }
+
+    private void btn_Decrypt_Click(object sender, EventArgs e)
+    {
+        var n = Convert.ToInt32(txtPrivatKey1);
+        var d = Convert.ToInt32(txtPrivatKey2);
+        var vIntervall = n.ToString().Length;
+
+
+        var el = Math.Sqrt((double)n);
+        var vT = txtDecyrpt.Text;
+        var splitvt = String.Split(vT, vIntervall);
+
+        var rvListNum = new List<char>();
+        var rvlist = new List<string>();
+
+        foreach (var c in splitvt)
         {
-            InitializeComponent();
-
-            //Zum Entschlüsseln: 102 341 418;
-            //Privatekey(19, 481);
+            var cNum = Convert.ToInt32(c);
+         
+            rvListNum.Add((char)(BigInteger.ModPow(cNum, d, n) + 54));
         }
+    }
 
-        private void btn_Decrypt_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btn_Encrypt_Click(object sender, EventArgs e)
-        {
-
-        }
+    private void btn_Encrypt_Click(object sender, EventArgs e)
+    {
     }
 }
